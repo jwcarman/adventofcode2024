a6050e7a88a8a0075b8a317a0c71c69f3631b057
James Carman <jcarman@callibrity.com>
2024-12-02 08:20:17 -0500

    Updating Copyright

--- src/main/kotlin/adventofcode/util/grid/Grids.kt
+++ src/main/kotlin/adventofcode/util/grid/Grids.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 James Carman
+ * Copyright (c) 2024 James Carman
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
